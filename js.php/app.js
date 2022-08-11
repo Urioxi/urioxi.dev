ae824bd028ba051f7db3253bd4a1794457525fd3
Urioxi <urioxa@gmail.com>
2022-08-11 14:17:23 +0200

    add5

---
+++ js.php/app.js
@@ -0,0 +1,17 @@
+const btnToggle = document.querySelector('.btn-toggle');
+
+btnToggle.addEventListener('click', () => {
+const body = document.body;
+
+if(body.classList.contains('dark')){
+    body.classList.add('light')
+    body.classList.remove('dark')
+    btnToggle.innerHTML = "Dark"
+} else if(body.classList.contains('light')){
+    body.classList.add('dark')
+    body.classList.remove('light')
+    btnToggle.innerHTML = "Light"
+    var svgg = document.getElementById("svgg");
+}
+
+})
