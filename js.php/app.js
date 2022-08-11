83f973c6776e3af2ed151cd747d86a9f108b9e71
Urioxi <urioxa@gmail.com>
2022-08-11 17:16:56 +0200

    add9

--- js.php/app.js
+++ js.php/app.js
@@ -6,17 +6,10 @@ const body = document.body;
 if(body.classList.contains('dark')){
     body.classList.add('light')
     body.classList.remove('dark')
-    btnToggle.innerHTML = display_image('sun.png',
-    276,
-    110,
-    'JavaScriptImage');
+    btnToggle.innerHTML = ('')
 } else if(body.classList.contains('light')){
     body.classList.add('dark')
     body.classList.remove('light')
-    btnToggle.innerHTML = display_image('moon.png',
-    276,
-    110,
-    'JavaScriptImage');
-}
+    btnToggle.innerHTML = ('')
 
-})
+}})
